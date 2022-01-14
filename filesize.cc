0dc89f7826cf033af5e68abf13a7b0e0b567c516
ARAOLUWA31 <96535733+ARAOLUWA31@users.noreply.github.com>
2022-01-14 15:39:39 -0500

    Update filesize.cc

--- filesize.cc
+++ filesize.cc
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <fstream>
-#include <string>
 using namespace std;
 
 // output format to use IDENTICALLY:
@@ -11,25 +10,26 @@ using namespace std;
 //
 int main( int argc, char* argv[] )
 {
-    ifstream myfile;
-    string tmp;
-    std::cout << "program: " << argv[0] << endl;
-    
-    for (int arg = 1; arg < argc; arg++){
-        int counter = 0;
-       
-        myfile.open(argv[arg]);
-        if(myfile.is_open()){
-            while(!(myfile.eof())){
-            //instead of trying to track '\n' and endl;, just use getline til you cant and count that
-            getline(myfile, tmp);
-            counter++;
+    // just to get you started, this is how to refer to the arguments that were passed
+    cout << "program: " << argv[0] << endl;
+    ifstream ins;
+
+    for (int arg = 1; arg < argc; ++arg){
+            int lines = 0;
+            string str;
+            ins.open(argv[arg]);
+
+            if(ins.fail())
+                lines = -1;
+            else{
+                while(getline(ins, str)){
+                    lines++;
+                }
             }
-            cout << " " << argv[arg]<< ": " << counter << endl;
-            myfile.close();
-        }else{
-            cout << argv[arg] << ": " << "-1" << endl;
+            ins.close();
+            std::cout << " " << argv[arg] << ":" << " " << lines << "\n";
 
     }
-    exit(0); 
+
+    exit(0); // this means that the program executed correctly!
 }
